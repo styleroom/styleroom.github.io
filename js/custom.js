c99956f553b80a529c2f0bf3370bdea49b4ac6b1
styleroom <styleroom@yandex.ru>
2016-11-05 15:25:27 +0300

    новый сайдбар col-md-3

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,6 @@ $(document).ready(function(){
     $('meta[name="description"]').attr('content',$('.description').text().trim().replace("\n", ""));
     $('#template_menu').load('../inc/menu.html');
     $('#template_sidebar').load('../inc/sidebar.html');
+    $('#template_sidebar_3').load('../inc/sidebar_3.html');
     $('#template_footer').load('../inc/footer.html');
 });

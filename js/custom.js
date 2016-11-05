33782197b2d2db2b9ffb283cb03eba700ab7d61d
styleroom <styleroom@yandex.ru>
2016-11-05 12:24:08 +0300

    переименование ainc в inc + замена путей

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('title').html($('h1').html());
     $('meta[name="description"]').attr('content',$('.description').text().trim().replace("\n", ""));
-    $('#template_menu').load('../ainc/menu.html');
-    $('#template_sidebar').load('../ainc/sidebar.html');
-    $('#template_footer').load('../ainc/footer.html');
+    $('#template_menu').load('../inc/menu.html');
+    $('#template_sidebar').load('../inc/sidebar.html');
+    $('#template_footer').load('../inc/footer.html');
 });
